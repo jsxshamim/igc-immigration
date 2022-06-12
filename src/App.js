e5b89e1763a307a381aa658a429b0d9820c17542
Shamim Reza <wpshamim@gmail.com>
2022-06-12 16:54:34 +0600

    Implement React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Components/Pages/Home/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import About from "./Components/Pages/About/About";
+import Blog from "./Components/Pages/Blog/Blog";
+import Login from "./Components/Pages/User/Login/Login";
+import Signup from "./Components/Pages/User/Signup/Signup";
+import Header from "./Components/Shared/Header/Header/Header";
+import Footer from "./Components/Shared/Footer/Footer";
+import NotFound from "./Components/Shared/NotFound/NotFound";
+import Checkout from "./Components/Pages/Checkout/Checkout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header></Header>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/Signup" element={<Signup />} />
+
+                <Route path="/checkout" element={<Checkout />} />
+
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
